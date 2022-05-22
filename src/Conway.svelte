8ea8c1d58968162733f4a89d9d6ba6f2b50952a1
Nicholas Hazel <nicholas.hazel@gmail.com>
2022-05-22 15:24:46 -0700

    feat: added basic landscape

--- src/Conway.svelte
+++ src/Conway.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   /* TODO
-  [ ] Generate programmatic grid (Landscape)
+  [X] Generate programmatic grid (Landscape)
   [ ] User input (setup initial Landscape)
   [ ] Toggle cell (boolean) on Landscape
 
@@ -15,29 +15,64 @@
   // Represents single Cell
   type Cell = boolean
   type Row = Array<Cell>
-  // Represents remapped Column
-  type Column = Array<Cell>
   // Represents natural Landscape (square)
-  type LandScape = Array<Row>
+  type Landscape = Array<Row>
+
+  const cells = 50
 
 /**
  * This generates a Landscape
  * @param cells - Number of cells in a Row or Column
  */
-  const createLandscape = (cells: number): LandScape => {
-    const row: Row = Array(cells)
-
-    const landscape: LandScape = row.map(cell => row)
-    console.log(landscape)
-    return landscape
+  const createLandscape = (cells: number): Landscape => {
+    const row = Array(cells).fill(false)
+    const landscape = Array(cells).fill(row)
+    const appliedLandscape = landscape.map(row =>
+      row.map(() => Math.random() > 0.85))
+    // Return booleans of booleans of booleans!
+    return appliedLandscape
   }
+
+  const landscape = createLandscape(cells)
 </script>
 
 <!-- Root level element containing any rendering for Conway -->
 <div id="wrapper">
-
+  <h1>
+    {#each landscape as row}
+      <div class="row">
+        {#each row as cell}
+          <span class="cell
+            {cell ? 'alive' : 'dead'}
+          ">{cell ? 'O' : 'X' }</span>
+        {/each}
+      </div>
+    {/each}
+  </h1>
 </div>
 
 <style>
-
+  h1 {
+    display: block;
+  }
+  .cell {
+    width: 1.5rem;
+    height: 1.5rem;
+    background: #CCC;
+    display: inline-block;
+    margin: -0.125rem 0.125rem;
+    font-size: 1rem;
+    line-height: 1rem;
+    color: #FEFEFE;
+  }
+  .alive {
+    background: #E0BBE4;
+  }
+  .dead {
+    background: #FFDFD3;
+  }
+  .row {
+    margin: 0;
+    padding: 0;
+  }
 </style>
